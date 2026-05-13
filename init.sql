cf6da932a668920ab2e6142b315e0057b8be5deb
pkjvish <vishpkj@gmail.com>
2026-05-13 13:46:10 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,15 +1,13 @@
-CREATE DATABASE IF NOT EXISTS sales_db;
-USE sales_db;
+CREATE DATABASE IF NOT EXISTS crud_db;
+USE crud_db;
 
-CREATE TABLE IF NOT EXISTS customers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+EATE TABLE IF NOT EXISTS tbl_user (
+                user_id INT AUTO_INCREMENT PRIMARY KEY,
+                user_name VARCHAR(45) NOT NULL,
+                user_email VARCHAR(45) NOT NULL);
 
 -- Insert sample data
-INSERT INTO customers (name, email) VALUES 
+INSERT INTO tbl_user (name, email) VALUES 
 ('Pankaj Kumar', 'pankaj@example.com'),
 ('Jane Doe', 'jane@example.com')
 ON DUPLICATE KEY UPDATE name=name;
