4f1f132f5c91344c958565bf98629d8945b8f00d
pkjvish <vishpkj@gmail.com>
2026-05-13 13:36:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,12 +36,15 @@ def set_db():
     try:
         cur = mysql.connection.cursor()
         print('connection start')
+        
         # 1. Create Database (if it doesn't exist)
         cur.execute("CREATE DATABASE IF NOT EXISTS crud_db")
-         print('DB created')
+        print('DB created')
+        
         # 2. Switch to the database
         cur.execute("USE crud_db")
-         print('using DB')
+        print('using DB')
+        
         # 3. Create Table
         cur.execute("""
             CREATE TABLE IF NOT EXISTS tbl_user (
@@ -50,7 +53,8 @@ def set_db():
                 user_email VARCHAR(45) NOT NULL
             )
         """)
-         print('table created')
+        print('table created')
+        
         mysql.connection.commit()
         cur.close()
         return jsonify({"message": "Database and Table created successfully!"}), 200
